2d1e14d675639ee7d91e86f89d26805e31c978da
Maicol Nuñez <110599461+MaicolNz@users.noreply.github.com>
2024-09-20 19:34:43 -0300

    BackendDeploy

--- src/main/java/com/PI/Back/PIBackend/config/WebConfig.java
+++ src/main/java/com/PI/Back/PIBackend/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {  // Cambiado a addCorsMappings
         registry.addMapping("/**")
-                .allowedOrigins("https://proyectofinal-omega.vercel.app")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
